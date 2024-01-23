6b681a02d4f1377e151ea8bafe8a8a374ec1cfd8
arimaulanahardan <arimaulanahardan@gmail.com>
2024-01-23 18:51:01 +0700

    add new icon footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
+import { FaFacebook, FaInstagramSquare, FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 import './footer.css'
 
 const Footer = () => {
     return (
         <section className='f-wrapper'>
             <div className="paddings innerWidth f-container">
+                <div className="flexCenter f-row">
+                    <a
+                    href="#">
+                        <FaFacebook
+                            size={40}
+                            color='var(--blue)'
+                        />
+                    </a>
+                    <a 
+                    href="#">
+                        <FaInstagramSquare
+                            size={40}
+                            color='var(--blue)'
+                        />
+                    </a>
+                    <a 
+                    href="#">
+                        <MdEmail
+                            size={40}
+                            color='var(--blue)'
+                        />
+                    </a>
+                    <a 
+                    href="#">
+                        <FaWhatsapp
+                            size={40}
+                            color='var(--blue)'
+                        />
+                    </a>
+                </div>
                 <div className="footer_bg">
                     <div className="footer_bg_one"></div>
                     <div className="footer_bg_two"></div>
